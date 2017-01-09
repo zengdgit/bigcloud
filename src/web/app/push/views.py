59261e814435ff77955292276794243d9350bf1d
Eleven <eleveneat@gmail.com>
2017-01-09 17:51:14 +0800

    确定「安装包」样式及完成获取与删除的逻辑
    - 确定表格样式，没有编辑按钮，状态有两种：「上传成功」以及「正在上传」。
    - 新建上传文件状态的Modal。
    - 后端完成获取与删除的逻辑。
    - 前端完成Resumable逻辑，未测试。

--- src/web/app/push/views.py
+++ src/web/app/push/views.py
@@ -6,6 +6,7 @@
 
 from flask import Flask, request, jsonify, render_template, url_for, redirect
 from . import push
+from ..models import Package
 from flask_login import login_required
 
 
@@ -20,22 +21,79 @@ def index():
 def littlecloud():
     return render_template('push/push_littlecloud.html')
 
+
 @push.route('/usergroup')
 @login_required
 def usergroup():
     return render_template('push/push_usergroup.html')
+
 
 @push.route('/application')
 @login_required
 def application():
     return render_template('push/push_application.html')
 
+
 @push.route('/package')
 @login_required
 def package():
     return render_template('push/push_package.html')
 
+
 @push.route('/classification_of_application')
 @login_required
 def classification_of_application():
     return render_template('push/classification_of_application.html')
+
+
+#################
+# package
+#################
+@push.route('/api/package', methods=['GET'])
+@login_required
+def get_all_packages():
+    '''
+    【API】得到所有安装包的数据。
+    :return: 安装包 ID
+    '''
+    packages = Package.query.all()
+    dic = []
+    for i in packages:
+        item = {
+            "id": i.id,
+            "filename": i.filename,
+            "relative_path": i.relative_path,
+            "identifier": i.identifier,
+            "total_size": i.total_size,
+            "total_chunks": i.total_chunks,
+            "is_complete": i.is_complete,
+            "md5": i.md5,
+        }
+        dic.append(item)
+    res = {"result": True, "data": dic, "message": u"Get all packages successfully!"}
+    return jsonify(res)
+
+
+@push.route('/api/package', methods=['POST'])
+@login_required
+def upload_package():
+    '''
+    【API】处理上传文件chunk。
+    '''
+    res = {"result": True, "data": None, "message": u"upload package successfully!"}
+
+
+@push.route('/api/package/<int:id>', methods=['DELETE'])
+@login_required
+def delete_package_by_id(id):
+    '''
+    【API】根据 id 删除安装包。
+    :param id: 安装包 ID
+    :return:
+    '''
+    package = Package.query.get(int(id))
+    if package:
+        package.delete()
+        return jsonify({"result": True, "data": None, "message": "Delete the package successfully!"})
+    res_message = u"Failed! The package with id %s is not excisted!" % id
+    return jsonify({"result": False, "data": None, "message": res_message})
