af0b20c744313c71744125c96f6a144a45ac6daa
Eleven <eleveneat@gmail.com>
2017-01-19 11:57:26 +0800

    完成「注册应用信息」页面
    - 完成添加与删除功能。
    - 添加ApplicationForm对象。
    - 完善下拉框逻辑。

--- src/web/app/push/forms.py
+++ src/web/app/push/forms.py
@@ -4,10 +4,27 @@
 # 2016/12/22 Chen Weijian : Init
 
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.validators import DataRequired, Length
 from .. import upload
+
 
 class UploadForm(FlaskForm):
     upload_file = FileField(validators=[
         FileAllowed(upload, u'只能上传图片！'),
         FileRequired(u'文件未选择！')])
+
+
+class ApplicationForm(FlaskForm):
+    name = StringField(id='name', validators=[DataRequired(), Length(1, 50)])
+    first_classification_id = StringField(id='first_classification_id', validators=[DataRequired(), Length(1, 10)])
+    secondary_classification_id = StringField(id='secondary_classification_id',
+                                              validators=[DataRequired(), Length(1, 10)])
+    function_id = StringField(id='function_id', validators=[DataRequired(), Length(1, 10)])
+    language_id = StringField(id='language_id', validators=[DataRequired(), Length(1, 255)])
+    os_id = StringField(id='os_id', validators=[DataRequired(), Length(1, 255)])
+    cpu_id = StringField(id='cpu_id', validators=[DataRequired(), Length(1, 255)])
+    package_id = StringField(id='package_id', validators=[DataRequired(), Length(1, 255)])
+    install_command = StringField(id='install_command', validators=[DataRequired(), Length(1, 2083)])
+    version = StringField(id='version', validators=[DataRequired(), Length(1, 255)])
--- src/web/app/push/views.py
+++ src/web/app/push/views.py
@@ -9,7 +9,7 @@ from . import push
 from ..models import Package, Application, OS, Language, CPU, FirstClassification, SecondaryClassification, Function
 from ..utils import checksum
 from flask_login import login_required, current_user
-from .forms import UploadForm
+from .forms import UploadForm, ApplicationForm
 from .. import upload, logger
 
 
@@ -70,7 +70,7 @@ def get_all_packages():
             "md5": i.md5,
         }
         data.append(item)
-    logger.info("{0} - Get all packages".format(current_user.name))
+    # logger.info("{0} - Get all packages".format(current_user.name))
     res = {"result": True, "data": data, "message": u"Get all packages successfully!"}
     return jsonify(res)
 
@@ -234,17 +234,87 @@ def get_all_applications():
             "id": i.id,
             "name": i.name,
             "version": i.version,
+            "secondary_classification": i.function.secondary_classification.name if i.function else "",
             "function": i.function.name if i.function else "",
             "os": i.os.name if i.os else "",
             "cpu": i.cpu.name if i.cpu else "",
             "language": i.language.name if i.language else "",
             "install_command": i.install_command,
-            "package": i.package.name if i.package else "",
+            "package": i.package.filename if i.package else "",
+            "first_classification_id": i.function.secondary_classification.first_classification.id if i.function else "",
+            "secondary_classification_id": i.function.secondary_classification.id if i.function else "",
+            "function_id": i.function_id,
+            "os_id": i.os_id,
+            "cpu_id": i.cpu_id,
+            "language_id": i.language_id,
+            "package_id": i.package_id,
         }
         data.append(item)
     # logger.info("{0} - Get all applications".format(current_user.name))
     res = {"result": True, "data": data, "message": u"Get all applications successfully!"}
     return jsonify(res)
+
+
+@push.route('/api/application', methods=['POST'])
+@login_required
+def add_application():
+    '''
+    【API】添加应用。
+    :return:
+    '''
+    form = ApplicationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        new_application = Application(
+            name=form.name.data,
+            version=form.version.data,
+            function_id=form.function_id.data,
+            language_id=form.language_id.data,
+            cpu_id=form.cpu_id.data,
+            os_id=form.os_id.data,
+            package_id=form.package_id.data,
+            install_command=form.install_command.data,
+        )
+        new_application.save()
+        logger.info("{0} - Add {1} application with id {2}".format(current_user.name, name, new_application.id))
+        return jsonify({"result": True, "data": None, "message": "Add the application successfully"})
+    err = form.errors
+    logger.error("{0} - Fail to add application because {1}".format(current_user.name, err))
+    res = {"result": True, "data": None, "message": err}
+    return jsonify(res)
+
+
+@push.route('/api/application/<int:id>', methods=['PUT'])
+@login_required
+def update_application_by_id(id):
+    '''
+    【API】根据 id 更新应用。
+    :param id: 应用 ID
+    :return:
+    '''
+    form = ApplicationForm()
+    if form.validate_on_submit():
+        application = Application.query.get(int(id))
+        if application:
+            application.name = form.name.data
+            application.version = form.version.data
+            application.function_id = form.function_id.data
+            application.language_id = form.language_id.data
+            application.cpu_id = form.cpu_id.data
+            application.os_id = form.os_id.data
+            application.package_id = form.package_id.data
+            application.install_command = form.install_command.data
+            application.save()
+            logger.info(
+                "{0} - Update {1} application with id {2}".format(current_user.name, application.name, application.id))
+            return jsonify({"result": True, "data": None, "message": u"Edit new application successfully"})
+        res_message = u"Failed! The application with id %s is not excisted" % id
+        logger.error("{0} - {1}".format(current_user.name, res_message))
+        return jsonify({"result": False, "data": None, "message": res_message})
+
+    error = form.errors
+    logger.error("{0} - Fail to update application because {1}".format(current_user.name, error))
+    return jsonify({"result": False, "data": None, "message": error})
 
 
 @push.route('/api/application/<int:id>', methods=['DELETE'])
